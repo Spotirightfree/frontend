ae864efba804be4d95c3b493889274fee046eab3
BillyKuiper <456181@student.fontys.nl>
2023-05-10 16:17:41 +0200

    change frontpage

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welkom bij mijn React App!
+        Welkom bij mijn React App! In Kubernetes!
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
